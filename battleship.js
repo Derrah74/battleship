5baeb9a2bd4f3427b321d39133a41f18698548d8
Derrah74 <derrah74@hotmail.com>
2017-02-07 20:13:34 -0800

    Add files via upload

---
+++ battleship.js
@@ -0,0 +1,31 @@
+// JavaScript Document
+
+var randomLoc = Math.floor(Math.random() *5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+
+while (isSunk == false) {
+  guess = prompt("Ready, aim, fire! Enter your number one thru six:");
+  if (guess < 0 || guess > 6) {
+    alert("Please enter a valid number!");  
+  } else {
+    guesses = guesses + 1;
+    if (guess == location1 || guess == location2 || guess == location3) {
+      alert("Nice. Hit!");
+      hits = hits + 1;
+      if (hits == 3) {
+        isSunk = true;
+        alert("Boom! You sank my battleship!");
+      }
+    } else {
+      alert("Haha Miss!");
+    }
+  }
+}
+
+var stats = "You took " + guesses + " guesses to sink the ship, good job. " + "Your accuracy was " + (3/guesses); alert(stats);
